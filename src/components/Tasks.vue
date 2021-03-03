e7a192c4ee6961e2fdd02edd010e945cca8e8327
SpykeRel04D <roger.vidal.lloveras@gmail.com>
2021-03-03 12:43:36 +0100

    Start working with components (pass data and props)

---
+++ src/components/Tasks.vue
@@ -0,0 +1,18 @@
+<template>
+	<div v-for="task in tasks" v-bind:key="task.id">
+		<Task :task="task" />
+	</div>
+</template>
+
+<script>
+import Task from "./Task";
+export default {
+	name: "Tasks",
+	components: {
+		Task,
+	},
+	props: {
+		tasks: Array,
+	},
+};
+</script>
